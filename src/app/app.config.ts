b6fc25f0a6e788b4a07b2a674e8d389b43653588
Victor Antonio Hernandez Nuñez <victor.hernandezn@outlook.com>
2025-04-14 12:19:15 +0200

    chore(app): install angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule), provideAnimationsAsync()
+  ]
+}
